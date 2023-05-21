08ee14984ec9c7e6dbef195641bf6cc75afd4d82
LauraAmaliaCM <44227418+LauraAmaliaCM@users.noreply.github.com>
2023-05-21 08:31:44 +0200

    Update loadingScene.js

--- js/scenes/loadingScene.js
+++ js/scenes/loadingScene.js
@@ -67,9 +67,14 @@ class loadingScene extends Phaser.Scene {
             percentText.destroy();
             assetText.destroy();
         });
-
-
-
+        
+        // Assets de la gameScene
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+        
     }
 
     create() {
